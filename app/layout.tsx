8f31ac201081bfa71dd62a975a67ec1e5ec434ec
eliamiguel <eliasperreiramiguel@gmail.com>
2025-07-13 16:03:59 -0300

    ajustar layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Inter } from "next/font/google"
+import { usePathname } from "next/navigation"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ReactQueryProvider } from "@/lib/react-query-provider"
@@ -15,6 +16,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pathname = usePathname()
+  const isLoginPage = pathname === "/login"
+
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
@@ -25,14 +29,18 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <ReactQueryProvider>
-            <SidebarProvider>
-              <div className="flex min-h-screen w-full">
-                <AppSidebar />
-                <main className="flex-1 bg-gray-50">
-                  {children}
-                </main>
-              </div>
-            </SidebarProvider>
+            {isLoginPage ? (
+              children
+            ) : (
+              <SidebarProvider>
+                <div className="flex min-h-screen w-full">
+                  <AppSidebar />
+                  <main className="flex-1 bg-gray-50">
+                    {children}
+                  </main>
+                </div>
+              </SidebarProvider>
+            )}
             <Toaster />
           </ReactQueryProvider>
         </ThemeProvider>
