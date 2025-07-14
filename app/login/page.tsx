5544898e42eb7515e93b5fee3ee437c184b6394e
eliamiguel <eliasperreiramiguel@gmail.com>
2025-07-14 12:02:04 -0300

    upadte acios e page login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -58,7 +58,7 @@ export default function LoginPage() {
     setError("")
 
     try {
-      const response = await fetch("http://localhost:8000/api/auth/login", {
+        const response = await fetch(`https://app-crm-academy-back.onrender.com/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- axios.ts
+++ axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL:  `${process.env.NEXT_PUBLIC_BACKEND_URL}`,
+  baseURL:  `${process.env.NEXT_PUBLIC_BACKEND_URL}` || "https://app-crm-academy-back.onrender.com/api",
 })
 
 // Interceptor para adicionar o token de autenticação automaticamente
